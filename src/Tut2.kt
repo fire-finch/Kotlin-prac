380852b436bb4df7857d56a8a6fb2d9393c54d7d
fire-finch <firefinchdev@gmail.com>
2018-05-22 19:55:08 +0530

    Strings

---
+++ src/Tut2.kt
@@ -0,0 +1,5 @@
+fun main(args: ArrayList<String>) {
+    println("3.14 to Int: " + 3.14.toInt())
+    println("A to int: ${'A'.toChar()}")
+    println("65 to Char: ${65.toChar()}")
+}
---
+++ src/strings.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val name = "anirudh"
+    val longStr = """This is a
+long string"""
+
+    var name2: String = "ani"
+
+    println("Name: $name")
+
+    println("Strings Equal: ${"ABC".equals("ABC")}")
+
+    println("Compare A and B: ${"A".compareTo("B")}")
+}
