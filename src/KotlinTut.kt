30584fc25141636255bb75d1483d0b5fe1371d89
fire-finch <firefinchdev@gmail.com>
2018-05-22 19:46:50 +0530

    1

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -2,4 +2,19 @@ package demo
 
 fun main(args: Array<String>) {
     println("Hello World")
+
+    val name = "Anirudh"
+    var age = 20
+
+    var bigInt: Int = Int.MAX_VALUE
+
+    println("Yoo $bigInt")
+
+    if(true is Boolean) {
+        print("true is boolean\n")
+    }
+
+    var letter: Char = 'A'
+    println("A is a Char: ${letter is Char}")
+
 }
